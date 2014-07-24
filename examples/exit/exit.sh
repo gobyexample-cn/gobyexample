3d98d103c43e16179bc41ecb1d0241ff3c018a70
everyx <lunt.luo@gmail.com>
2014-07-25 00:28:51 +0800

    fixed #3

--- examples/exit/exit.sh
+++ examples/exit/exit.sh
@@ -1,10 +1,9 @@
-# 如果你使用 `go run` 来运行 `exit.go`，那么退出状态将会倍 `go`
+# 如果你使用 `go run` 来运行 `exit.go`，那么退出状态将会被 `go`
 # 捕获并打印。
 $ go run exit.go
 exit status 3
 
-# 使用编译并执行一个二进制文件的方式，你可以在终端中查
-# 看退出转台。
+# 使用编译并执行一个二进制文件的方式，你可以在终端中查看退出状态。
 $ go build exit.go
 $ ./exit
 $ echo $?
