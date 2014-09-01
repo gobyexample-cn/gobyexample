41d08b46928be78b9aaa959e92e438ea9d3096f5
everyx <lunt.luo@gmail.com>
2014-09-01 22:43:23 +0800

    修正 markdown 链接错误

--- examples/recursion/recursion.go
+++ examples/recursion/recursion.go
@@ -1,4 +1,4 @@
-// Go 支持 [_递归_](http://zh.wikipedia.org/wiki/%E9%80%92%E5%BD%92_(%E8%AE%A1%E7%AE%97%E6%9C%BA%E7%A7%91%E5%AD%A6)).
+// Go 支持 <a href="http://zh.wikipedia.org/wiki/%E9%80%92%E5%BD%92_(%E8%AE%A1%E7%AE%97%E6%9C%BA%E7%A7%91%E5%AD%A6)"><em>递归</em></a>.
 // 这里是一个经典的阶乘示例。
 
 package main
