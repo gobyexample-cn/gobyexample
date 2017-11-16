dc9c04163011984ecb58dd17cd7699892306bccf
Xingan Wang <wangxgwxg@gmail.com>
2017-11-16 15:32:28 -0800

    fix typo
    fix pr#29, #26

--- examples/defer/defer.go
+++ examples/defer/defer.go
@@ -11,9 +11,9 @@ import "os"
 // 时关闭它。这里展示了如何通过 `defer` 来做到这一切。
 func main() {
 
-    // 在 `closeFile` 后得到一个文件对象，我们使用 defer
-    // 通过 `closeFile` 来关闭这个文件。这会在封闭函数
-    // （`main`）结束时执行，就是 `writeFile` 结束后。
+    // 在 `createFile` 后得到一个文件对象，
+    // 我们使用 defer 通过 `closeFile` 来关闭这个文件。
+    // 这会在封闭函数 （`main`）结束时执行，就是 `writeFile` 结束后。
     f := createFile("/tmp/defer.txt")
     defer closeFile(f)
     writeFile(f)
