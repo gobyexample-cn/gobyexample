30715e17f2a192d4067960a7e741828eda89f592
Xingan Wang <wangxgwxg@gmail.com>
2017-11-27 21:48:33 -0800

    values, variables, constants update

--- examples/values/values.go
+++ examples/values/values.go
@@ -1,4 +1,4 @@
-// Go 拥有各值类型，包括字符串，整形，浮点型，布尔
+// Go 拥有多种值类型，包括字符串，整形，浮点型，布尔
 // 型等。下面是一些基本的例子。
 
 package main
@@ -7,15 +7,15 @@ import "fmt"
 
 func main() {
 
-    // 字符串可以通过 `+` 连接。
-    fmt.Println("go" + "lang")
+	// 字符串可以通过 `+` 连接。
+	fmt.Println("go" + "lang")
 
-    // 整数和浮点数
-    fmt.Println("1+1 =", 1+1)
-    fmt.Println("7.0/3.0 =", 7.0/3.0)
+	// 整数和浮点数
+	fmt.Println("1+1 =", 1+1)
+	fmt.Println("7.0/3.0 =", 7.0/3.0)
 
-    // 布尔型，还有你想要的逻辑运算符。
-    fmt.Println(true && false)
-    fmt.Println(true || false)
-    fmt.Println(!true)
+	// 布尔型，以及常见的布尔操作。
+	fmt.Println(true && false)
+	fmt.Println(true || false)
+	fmt.Println(!true)
 }
--- examples/variables/variables.go
+++ examples/variables/variables.go
@@ -1,5 +1,5 @@
-// 在 Go 中，_变量_ 被显式声明，并被编译器所用来
-// 检查函数调用时的类型正确性
+// 在 Go 中，_变量_ 被显式声明，并可以被编译器用来
+// 检查函数调用时的类型正确性。
 
 package main
 
@@ -7,25 +7,25 @@ import "fmt"
 
 func main() {
 
-    // `var` 声明 1 个或者多个变量。
-    var a string = "initial"
-    fmt.Println(a)
+	// `var` 声明 1 个或者多个变量。
+	var a string = "initial"
+	fmt.Println(a)
 
-    // 你可以申明一次性声明多个变量。
-    var b, c int = 1, 2
-    fmt.Println(b, c)
+	// 你可以一次性声明多个变量。
+	var b, c int = 1, 2
+	fmt.Println(b, c)
 
-    // Go 将自动推断已经初始化的变量类型。
-    var d = true
-    fmt.Println(d)
+	// Go 将自动推断已经初始化的变量类型。
+	var d = true
+	fmt.Println(d)
 
-    // 声明变量且没有给出对应的初始值时，变量将会初始化为
-    // _零值_ 。例如，一个 `int` 的零值是 `0`。
-    var e int
-    fmt.Println(e)
+	// 声明后却没有给出对应的初始值时，变量将会初始化为
+	// _零值_ 。例如，一个 `int` 的零值是 `0`。
+	var e int
+	fmt.Println(e)
 
-    // `:=` 语句是申明并初始化变量的简写，例如
-    // 这个例子中的 `var f string = "short"`。
-    f := "short"
-    fmt.Println(f)
+	// `:=` 语法是声明并初始化变量的简写，例如
+	// 这个例子中的 `var f string = "short"`。
+	f := "short"
+	fmt.Println(f)
 }
