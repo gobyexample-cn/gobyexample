4e69a86c5d60bc500ca8d48451085caa7d99f87a
Alice <imqksl@gmail.com>
2019-11-15 17:00:14 +0800

    translation: waitgroups

--- examples/waitgroups/waitgroups.go
+++ examples/waitgroups/waitgroups.go
@@ -1,5 +1,4 @@
-// To wait for multiple goroutines to finish, we can
-// use a *wait group*.
+// 想到等待多个协程完成，我们可以使用 *wait group*。
 
 package main
 
@@ -9,34 +8,30 @@ import (
 	"time"
 )
 
-// This is the function we'll run in every goroutine.
-// Note that a WaitGroup must be passed to functions by
-// pointer.
+// 每个协程都会运行该函数。
+// 注意，WaitGroup 必须通过指针传递给函数。
 func worker(id int, wg *sync.WaitGroup) {
 	fmt.Printf("Worker %d starting\n", id)
 
-	// Sleep to simulate an expensive task.
+	// 睡眠一秒钟，以此来模拟耗时的任务。
 	time.Sleep(time.Second)
 	fmt.Printf("Worker %d done\n", id)
 
-	// Notify the WaitGroup that this worker is done.
+	// 通知 WaitGroup ，当前协程的工作已经完成。
 	wg.Done()
 }
 
 func main() {
 
-	// This WaitGroup is used to wait for all the
-	// goroutines launched here to finish.
+	// 这个 WaitGroup 被用于等待该函数开启的所有协程。
 	var wg sync.WaitGroup
 
-	// Launch several goroutines and increment the WaitGroup
-	// counter for each.
+	// 开启几个协程，并为其递增 WaitGroup 的计数器。
 	for i := 1; i <= 5; i++ {
 		wg.Add(1)
 		go worker(i, &wg)
 	}
 
-	// Block until the WaitGroup counter goes back to 0;
-	// all the workers notified they're done.
+	// 阻塞，直到 WaitGroup 计数器恢复为 0，即所有协程的工作都已经完成。
 	wg.Wait()
 }
