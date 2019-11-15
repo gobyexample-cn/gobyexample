cdc7ad3e223c67bd4c790d565b6f906d5fe12e34
Alice <imqksl@gmail.com>
2019-11-15 14:37:36 +0800

    translation: http-clients

--- examples/http-clients/http-clients.go
+++ examples/http-clients/http-clients.go
@@ -1,7 +1,6 @@
-// The Go standard library comes with excellent support
-// for HTTP clients and servers in the `net/http`
-// package. In this example we'll use it to issue simple
-// HTTP requests.
+// Go 标准库的 `net/http` 包为 HTTP 客户端和服务端提供了出色的支持。
+// 在这个例子中，我们将使用它发送简单的 HTTP 请求。
+
 package main
 
 import (
@@ -12,21 +11,19 @@ import (
 
 func main() {
 
-	// Issue an HTTP GET request to a server. `http.Get` is a
-	// convenient shortcut around creating an `http.Client`
-	// object and calling its `Get` method; it uses the
-	// `http.DefaultClient` object which has useful default
-	// settings.
+		// 向服务端发送一个 HTTP GET 请求。
+	// `http.Get` 是创建 `http.Client` 对象并调用其 `Get` 方法的快捷方式。
+	// 它使用了 `http.DefaultClient` 对象及其默认设置。
 	resp, err := http.Get("http://gobyexample.com")
 	if err != nil {
 		panic(err)
 	}
 	defer resp.Body.Close()
 
-	// Print the HTTP response status.
+	// 打印 HTTP response 状态.
 	fmt.Println("Response status:", resp.Status)
 
-	// Print the first 5 lines of the response body.
+	// 打印 response body 前面 5 行的内容。
 	scanner := bufio.NewScanner(resp.Body)
 	for i := 0; scanner.Scan() && i < 5; i++ {
 		fmt.Println(scanner.Text())
