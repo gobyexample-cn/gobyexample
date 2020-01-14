5252cb799a6aa6eeca69c3df1f7b91c79d32fa38
gorda <imqksl@gmail.com>
2020-01-14 15:17:54 +0800

    update select

--- examples/select/select.sh
+++ examples/select/select.sh
@@ -1,9 +1,7 @@
-# 我们首先接收到值 `"one"`，然后就是预料中的 `"two"`
-# 了。
+# 跟预期的一样，我们首先接收到值 `"one"`，然后是 `"two"`。
 $ time go run select.go 
 received one
 received two
 
-# 注意从第一次和第二次 `Sleeps` 并发执行，总共仅运行了
-# 两秒左右。
+# 注意，程序总共仅运行了两秒左右。因为 1 秒 和 2 秒的 `Sleeps` 是并发执行的，
 real	0m2.245s
