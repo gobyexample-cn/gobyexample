e643006b0970fcd42e9dceba029bf06e595de4cb
Tim Chen <31817670+TimzChen@users.noreply.github.com>
2020-08-12 13:44:08 +0800

    优化中文注释  (#65)
    * 优化中文注释
    
    * 优化中文注释
    
    * Update examples/xml/xml.go
    
    修正拼写错误
    
    Co-authored-by: 2BFL <1@linux.com>
    
    Co-authored-by: 2BFL <1@linux.com>

--- examples/xml/xml.go
+++ examples/xml/xml.go
@@ -7,11 +7,11 @@ import (
 	"fmt"
 )
 
-// 该类型将被映射为 XML。
-// 与 JSON 例子类似，字段 tag 包含了编码和解码的指令。
-// 这里我们使用了 XML 包的一些特性：
-// `XMLName` 字段名规定了 struct 的 XML 元素的名称；
-// `id,attr` 意思是 `Id` 字段是一个 XML _attribute_，而不是嵌套元素。
+//  `Plant` 结构将被映射到 `XML` 。
+// 与 `JSON` 示例类似，字段标签包含用于编码器和解码器的指令。
+// 这里我们使用了 `XML` 包的一些特性：
+//  `XMLName` 字段名称规定了该结构的 `XML` 元素名称；
+//  `id,attrr` 表示 `Id` 字段是一个 `XML` 属性，而不是嵌套元素。
 type Plant struct {
 	XMLName xml.Name `xml:"plant"`
 	Id      int      `xml:"id,attr"`
@@ -28,7 +28,7 @@ func main() {
 	coffee := &Plant{Id: 27, Name: "Coffee"}
 	coffee.Origin = []string{"Ethiopia", "Brazil"}
 
-	// 传入我们声明了 XML 的 plant 类型。
+	// 传入我们声明了 XML 的 Plant 类型。
 	// 使用 `MarshalIndent` 生成可读性更好的输出结果。
 	out, _ := xml.MarshalIndent(coffee, " ", "  ")
 	fmt.Println(string(out))
@@ -36,8 +36,8 @@ func main() {
 	// 明确的为输出结果添加一个通用的 XML 头部信息。
 	fmt.Println(xml.Header + string(out))
 
-	// 使用 `Unmarshal` 将 XML 格式的字节流解析到 struct 内。
-	// 如果 XML 格式不正确，或无法映射到 struct，将会返回一个描述性错误。
+	// 使用 `Unmarshal` 将 `XML` 格式字节流解析到 `Plant` 结构。
+	// 如果 `XML` 格式错误或无法映射到 `Plant` 结构，将返回一个描述性错误。
 	var p Plant
 	if err := xml.Unmarshal(out, &p); err != nil {
 		panic(err)
@@ -47,7 +47,7 @@ func main() {
 	tomato := &Plant{Id: 81, Name: "Tomato"}
 	tomato.Origin = []string{"Mexico", "California"}
 
-	// `parent>child>plant` 字段标签告诉编码器嵌套 `<parent><child>...` 下面的所有 plant。
+	// `parent>child>plant` 字段标签告诉编码器，将 `Plants` 中的元素嵌套在 `<parent><child>` 里面。
 	type Nesting struct {
 		XMLName xml.Name `xml:"nesting"`
 		Plants  []*Plant `xml:"parent>child>plant"`
