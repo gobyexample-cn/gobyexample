4d3b31d463c6a0bdfe9fa6cc9a9c2d26db565d23
lxt <lplusxt@qq.com>
2021-05-19 23:52:44 +0800

    Fix typo (#71)

--- examples/maps/maps.go
+++ examples/maps/maps.go
@@ -10,7 +10,7 @@ func main() {
 	// 要创建一个空 map，需要使用内建函数 `make`：`make(map[key-type]val-type)`。
 	m := make(map[string]int)
 
-	// 使用典型的 `make[key] = val` 语法来设置键值对。
+	// 使用典型的 `name[key] = val` 语法来设置键值对。
 	m["k1"] = 7
 	m["k2"] = 13
 
