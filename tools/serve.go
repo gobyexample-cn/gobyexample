1b78f0ff415c4e3a8ac2e4bb85655e45f573fc3f
everyx <lunt.luo@gmail.com>
2022-03-19 23:54:05 +0800

    fix: serve use .html file as fallback

--- tools/serve.go
+++ tools/serve.go
@@ -3,11 +3,28 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"os"
 )
+
+type HTMLDir struct {
+	d http.Dir
+}
+
+func (d HTMLDir) Open(name string) (http.File, error) {
+	f, err := d.d.Open(name)
+	if os.IsNotExist(err) {
+		if f, err := d.d.Open(name + ".html"); err == nil {
+			return f, nil
+		}
+	}
+	return f, err
+}
 
 func main() {
 	port := "8000"
 	publicDir := "public"
 	fmt.Printf("Serving Go by Example at http://127.0.0.1:%s\n", port)
-	http.ListenAndServe(":"+port, http.FileServer(http.Dir(publicDir)))
+	fs := http.FileServer(HTMLDir{http.Dir(publicDir)})
+	http.Handle("/", http.StripPrefix("/", fs))
+	http.ListenAndServe(":"+port, nil)
 }
